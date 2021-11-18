6ffd5c1bcfbb4f9f4ce085986b7309b460febb97
flaviocp90 <flaviocoppola90@gmail.com>
2021-11-18 17:26:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style/App.css";
+import Home from "./routes/home";
+import Navbar from "./components/navbar";
+import About from "./routes/about";
+import DisplayWindow from "./routes/display-window";
+import NotFound from './routes/not-found'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from "./components/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/vetrina' element={<DisplayWindow />}/>
+        <Route path='*' exact={true} element={<NotFound />}/>
+      </Routes>
+
+      <Footer />
+
+    </Router>
   );
 }
 
