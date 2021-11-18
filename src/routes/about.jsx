c519c92820c8f227835c181eecbb48a460419647
flaviocp90 <flaviocoppola90@gmail.com>
2021-11-18 22:04:55 +0100

    modufy about component

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../style/about.css";
-import about1 from '../img/about1.jpg';
+import about1 from '../img/about1.JPG';
 import about2 from '../img/about2.JPG';
 import about3 from '../img/about3.JPG';
 
