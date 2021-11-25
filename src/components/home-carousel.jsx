8cf4072b04a93ffa871ca296030adec108fb910e
flaviocp90 <flaviocoppola90@gmail.com>
2021-11-25 11:05:04 +0100

    modify photo order

--- src/components/home-carousel.jsx
+++ src/components/home-carousel.jsx
@@ -1,46 +1,29 @@
-import React from 'react';
-import '../style/home.css';
-import { Carousel } from 'react-bootstrap';
-import home1 from '../img/home1.jpg';
-import home2 from '../img/home2.jpg';
-import home3 from '../img/home3.jpg';
-
+import React from "react";
+import "../style/home.css";
+import { Carousel } from "react-bootstrap";
+import home1 from "../img/home1.jpg";
+import home2 from "../img/home2.jpg";
+import home3 from "../img/home3.jpg";
 
 const HomeCarousel = () => {
-    return (
-<Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100 mw-50"
-      src={home1}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={home2}
-      alt="Second slide"
-    />
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100 mw-50" src={home2} alt="First slide" />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={home3} alt="Second slide" />
 
-    <Carousel.Caption>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100 mw-50"
-      src={home3}
-      alt="Third slide"
-    />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100 mw-50" src={home1} alt="Third slide" />
 
-    <Carousel.Caption>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-
-    )
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 };
 
 export default HomeCarousel;
